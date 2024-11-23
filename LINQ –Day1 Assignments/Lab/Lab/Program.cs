80f309aa9674daf33d9c90494a836b654f6cd83e
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-11-23 21:53:31 +0200

    Linq Day1 Assignment update

--- LINQ –Day1 Assignments/Lab/Lab/Program.cs
+++ LINQ –Day1 Assignments/Lab/Lab/Program.cs
@@ -17,89 +17,90 @@ namespace Lab
         public string LastName { get; set; } 
         public Subject[] Subjects { get; set; } 
     }
-}
-internal class Program
+
+    internal class Program
     {
         static void Main(string[] args)
         {
 
-        #region 1
-        #region Query1
-        //List<int> list = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
+            #region 1
+            #region Query1
+            //List<int> list = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
 
-        //var distinctSortedNumbers = list.Distinct().OrderBy(x => x).ToList();
+            //var distinctSortedNumbers = list.Distinct().OrderBy(x => x).ToList();
 
-        //foreach (var x in distinctSortedNumbers)
-        //{
-        //    Console.WriteLine(x);
-        //}
-        #endregion
+            //foreach (var x in distinctSortedNumbers)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            #endregion
 
-        #region Query2
-        //var multipliedNumbers = distinctSortedNumbers.Select(n => new { n, Multi = n * n });
+            #region Query2
+            //var multipliedNumbers = distinctSortedNumbers.Select(n => new { n, Multi = n * n });
 
-        //foreach (var x in multipliedNumbers)
-        //{
-        //    Console.WriteLine($"Number = {x.n}, Multiply = {x.Multi}");
-        //}
-        #endregion
-        #endregion
-
-
-        #region 2
-
-        #region Query3
-        //string[] names = { "Tom", "Dick", "Harry", "MARY", "Jay" };
-
-        //var namesWithLen3 = names.Where(names => names.Length == 3);
-        //foreach (var name in namesWithLen3)
-        //{
-        //    Console.WriteLine(name);
-        //}
-        #endregion
-
-        #region Query4
-        //var namesWithA = names.Where(names => names.IndexOf('a', StringComparison.OrdinalIgnoreCase) >= 0).OrderBy(names => names.Length);
-
-        //foreach (var name in namesWithA) { Console.WriteLine(name); }
-        #endregion
-
-        #region Query5
-        //var first2Names = names.Take(2).ToList();
-
-        //foreach (var name in first2Names) { Console.WriteLine(name); }
-        #endregion
-
-        #endregion
+            //foreach (var x in multipliedNumbers)
+            //{
+            //    Console.WriteLine($"Number = {x.n}, Multiply = {x.Multi}");
+            //}
+            #endregion
+            #endregion
 
 
-        #region 3
-        #region Query6
-        //List<Student> students = new List<Student>() {
-        //    new Student() { ID = 1, FirstName = "Ali", LastName = "Mohammed", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 33, Name = "UML" } } },
-        //    new Student() { ID = 2, FirstName = "Mona", LastName = "Gala", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 34, Name = "XML" }, new Subject() { Code = 25, Name = "JS" } } },
-        //    new Student() { ID = 3, FirstName = "Yara", LastName = "Yousf", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 25, Name = "JS" } } },
-        //    new Student() { ID = 1, FirstName = "Ali", LastName = "Ali", Subjects = new Subject[] { new Subject() { Code = 33, Name = "UML" } } },
-        //};
+            #region 2
+
+            #region Query3
+            //string[] names = { "Tom", "Dick", "Harry", "MARY", "Jay" };
+
+            //var namesWithLen3 = names.Where(names => names.Length == 3);
+            //foreach (var name in namesWithLen3)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            #endregion
+
+            #region Query4
+            //var namesWithA = names.Where(names => names.IndexOf('a', StringComparison.OrdinalIgnoreCase) >= 0).OrderBy(names => names.Length);
+
+            //foreach (var name in namesWithA) { Console.WriteLine(name); }
+            #endregion
+
+            #region Query5
+            //var first2Names = names.Take(2).ToList();
+
+            //foreach (var name in first2Names) { Console.WriteLine(name); }
+            #endregion
+
+            #endregion
 
 
-        //var DisplayNameSub = students.Select(s => new { s.FirstName, s.LastName, s.Subjects.Length }).ToList();
-        //foreach (var student in students)
-        //{
-        //    Console.WriteLine($"Name = {student.FirstName} {student.LastName}, NumOfSubjects = {student.Subjects.Length}");
-        //}
-        #endregion
-
-        #region Query7
-        //var DisplayOrder = students.OrderByDescending(s => s.FirstName).ThenBy(s => s.LastName).ToList();
-
-        //foreach (var student in DisplayOrder)
-        //{
-        //    Console.WriteLine($"{student.FirstName} {student.LastName}");
-        //}
-        #endregion 
-        #endregion
+            #region 3
+            #region Query6
+            //List<Student> students = new List<Student>() {
+            //    new Student() { ID = 1, FirstName = "Ali", LastName = "Mohammed", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 33, Name = "UML" } } },
+            //    new Student() { ID = 2, FirstName = "Mona", LastName = "Gala", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 34, Name = "XML" }, new Subject() { Code = 25, Name = "JS" } } },
+            //    new Student() { ID = 3, FirstName = "Yara", LastName = "Yousf", Subjects = new Subject[] { new Subject() { Code = 22, Name = "EF" }, new Subject() { Code = 25, Name = "JS" } } },
+            //    new Student() { ID = 1, FirstName = "Ali", LastName = "Ali", Subjects = new Subject[] { new Subject() { Code = 33, Name = "UML" } } },
+            //};
 
 
+            //var DisplayNameSub = students.Select(s => new { s.FirstName, s.LastName, s.Subjects.Length }).ToList();
+            //foreach (var student in students)
+            //{
+            //    Console.WriteLine($"Name = {student.FirstName} {student.LastName}, NumOfSubjects = {student.Subjects.Length}");
+            //}
+            #endregion
+
+            #region Query7
+            //var DisplayOrder = students.OrderByDescending(s => s.FirstName).ThenBy(s => s.LastName).ToList();
+
+            //foreach (var student in DisplayOrder)
+            //{
+            //    Console.WriteLine($"{student.FirstName} {student.LastName}");
+            //}
+            #endregion
+            #endregion
+
+
+        }
     }
 }
