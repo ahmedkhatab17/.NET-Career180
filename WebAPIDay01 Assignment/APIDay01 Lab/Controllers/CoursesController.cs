db2438b7a98029782fb0bd77ac4f92b479393050
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-12-03 23:03:44 +0200

    Web API Day 1 Lab

---
+++ WebAPIDay01 Assignment/APIDay01 Lab/Controllers/CoursesController.cs
@@ -0,0 +1,37 @@
+﻿using APIDay01_Lab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIDay01_Lab.Controllers
+{
+    [Route("api/courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        static List<Course> courses = new List<Course>()
+        {
+            new Course() { Id = 1, Name = "CSS", Duration = 40 },
+            new Course() { Id = 2, Name = "HTML", Duration = 60 },
+            new Course() { Id = 3, Name = ".NET", Duration = 120 },
+        };
+
+        [HttpGet]
+        public List<Course> GetAll()
+        {
+            return courses;
+        }
+
+        [HttpGet("{name}")]
+        public Course GetByName(string name)
+        {
+            return courses.Find(s => s.Name == name);
+        }
+
+        [HttpPost]
+        public List<Course> Add(Course course)
+        {
+            courses.Add(course);
+            return courses;
+        }
+    }
+}
---
+++ WebAPIDay01 Assignment/APIDay01 Lab/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace APIDay01_Lab.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
