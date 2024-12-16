3ae787ed3ee056344c7021daf787a87dc9a8bcc5
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-12-16 16:25:36 +0200

    Web API Day 4 Lab

---
+++ WebAPIDay05 Assignment 4/Lab4/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using Lab4.Models;
+using Lab4.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController(CompanyContext db)
+        {
+            Db = db;
+        }
+
+        public CompanyContext Db { get; }
+
+        [HttpPost("add")]
+        public IActionResult AddEmployee([FromForm] EmployeeDTO edto, IFormFile photo)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = $"{Directory.GetCurrentDirectory()}\\Uploads\\{photo.FileName}";
+                FileStream st = new FileStream(path, FileMode.Create);
+                photo.CopyTo(st);
+
+                Employee e = new Employee()
+                {
+                    Name = edto.Name,
+                    Username = edto.Username,
+                    Salary = edto.Salary,
+                    Email = edto.Email,
+                    Password = edto.Password,
+                    Photo = path
+                };
+                Db.Employees.Add(e);
+                Db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+       
+
+        [HttpGet("all")]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = Db.Employees.ToList();
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            return Ok(Db.Employees.FirstOrDefault(e => e.Id == id));
+        }
+    }
+}
---
+++ WebAPIDay05 Assignment 4/Lab4/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab4.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        [Range(1200, 20000)]
+        public decimal Salary { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
